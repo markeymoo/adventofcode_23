cc2adb20e48488976423c4e6bb68c2c0a450503b
markeymoo <mark.prout@outlook.com>
2023-12-09 08:27:45 +0000

    Day 1

--- day1.py
+++ day1.py
@@ -1,39 +1,70 @@
 import re
 import logging
 
-logging.basicConfig()
-logging.root.setLevel(logging.INFO)
 
-file_input = open("day1_input.txt", "r")
-lines = file_input.read().splitlines()
+def createCalibrationValue(digit1: str, digit2: str):
+    combinedStr = digit1 + digit2
+    logging.info("Combined String: %s", combinedStr)
+    combinedInt = int(combinedStr)
+    logging.info("Combined Int: %d", combinedInt)
 
-logging.info("Lines Read: %s", lines)
-calibration_value = 0
+    return combinedInt
 
-for line in lines:
-    first_digit = ""
-    last_digit = ""
 
-    logging.info("Line To Process: %s", line)
+def openReadData(file_name: str):
+    logging.info("openReadData: %s", file_name)
+    file_input = open(file_name, "r")
+    lines = file_input.read().splitlines()
 
-    # search line for first number character
-    match = re.findall(r"[0-9]+", line)
-    logging.info("Matched Digits: %s", match)
+    logging.info("Lines Read: %s", lines)
+    return lines
 
-    # was a match found
-    if match is not None:
-        last_match = match.__len__()
-        logging.info("Len: %s", last_match)
 
-        first_digit = match[0]
+def init():
+    # configure logging
+    logging.basicConfig()
+    logging.root.setLevel(logging.INFO)
 
-        if last_match == 1:
-            last_digit = match[0]
-        else:
-            last_digit = match[last_match - 1]
+    # global variables
+    global calibration_value
+    calibration_value = 0
 
-    logging.info("First Digit: %s\nSecond Digit: %s", first_digit, last_digit)
 
-    # Adding digits together
-    calibration_value += int(first_digit) + int(last_digit)
-    logging.info("CalibrationValue: %d", calibration_value)
+def main():
+    init()
+    lines = openReadData("day1_input.txt")
+
+    # iterate through all lines
+    for line in lines:
+        first_digit = ""
+        last_digit = ""
+
+        logging.info("Line To Process: %s", line)
+
+        # search line for first number character
+        match = re.findall(r"\d{1}", line)
+        logging.info("Matched Digits: %s", match)
+
+        # was a match found
+        if match is not None:
+            last_match = match.__len__()
+            logging.info("Len: %s", last_match)
+
+            first_digit = match[0]
+
+            if last_match == 1:
+                last_digit = match[0]
+            else:
+                last_digit = match[last_match - 1]
+
+        logging.info("First Digit: %s\nSecond Digit: %s", first_digit, last_digit)
+
+        # Adding digits together
+        global calibration_value
+        calibration_value += createCalibrationValue(first_digit, last_digit)
+
+        logging.info("CalibrationValue: %d", calibration_value)
+
+
+if __name__ == "__main__":
+    main()
