1f6c041f58fa101bfc105b5a570ec87f075088cd
markeymoo <mark.prout@outlook.com>
2023-12-17 07:21:57 +0000

    Modified using Cody to review and fix SonarLint issues.

--- day1.py
+++ day1.py
@@ -2,39 +2,44 @@ import re
 import logging
 
 
-def convertToDigits(line: str):
-    line = re.sub(r"(?:oneight)", "oneeight", line, 0)
-    line = re.sub(r"(?:threeight)", "threeeight", line, 0)
-    line = re.sub(r"(?:fiveeight)", "fiveeight", line, 0)
-    line = re.sub(r"(?:nineight)", "nineeight", line, 0)
-    line = re.sub(r"(?:twone)", "twoone", line, 0)
-    line = re.sub(r"(?:sevenine)", "sevennine", line, 0)
-    line = re.sub(r"(?:eightwo)", "eighttwo", line, 0)
+def convert_to_digits(line: str):
+    line = line.replace("oneight", "oneeight") 
+    line = line.replace("threeight", "threeeight")
+    line = line.replace("fiveeight", "fiveeight")
+    line = line.replace("nineight", "nineeight")
+    line = line.replace("twone", "twoone")
+    line = line.replace("sevenine", "sevennine")
+    line = line.replace("eightwo", "eighttwo")
 
-    line = re.sub(r"(?:one)", "1", line, 0)
-    line = re.sub(r"(?:two)", "2", line, 0)
-    line = re.sub(r"(?:three)", "3", line, 0)
-    line = re.sub(r"(?:four)", "4", line, 0)
-    line = re.sub(r"(?:five)", "5", line, 0)
-    line = re.sub(r"(?:six)", "6", line, 0)
-    line = re.sub(r"(?:seven)", "7", line, 0)
-    line = re.sub(r"(?:eight)", "8", line, 0)
-    line = re.sub(r"(?:nine)", "9", line, 0)
+    line = line.replace("one", "1")
+    line = line.replace("two", "2") 
+    line = line.replace("three", "3")
+    line = line.replace("four", "4")
+    line = line.replace("five", "5")
+    line = line.replace("six", "6")
+    line = line.replace("seven", "7")
+    line = line.replace("eight", "8")
+    line = line.replace("nine", "9")
 
     return line
 
 
-def createCalibrationValue(digit1: str, digit2: str):
-    combinedStr = digit1 + digit2
-    logging.info("Combined String: %s", combinedStr)
-    combinedInt = int(combinedStr)
-    logging.info("Combined Int: %d", combinedInt)
-
-    return combinedInt
 
 
-def openReadData(file_name: str):
-    logging.info("openReadData: %s", file_name)
+
+def create_calibration_value(digit1: str, digit2: str):
+    combined_string = digit1 + digit2
+    logging.info("Combined String: %s", combined_string)
+    calibrated_value = int(combined_string)
+    logging.info("Combined Int: %d", calibrated_value)
+
+    return calibrated_value
+
+
+
+
+def read_data(file_name: str):
+    logging.info("read_data: %s", file_name)
     file_input = open(file_name, "r")
     lines = file_input.read().splitlines()
 
@@ -42,14 +47,15 @@ def openReadData(file_name: str):
     return lines
 
 
+
 def init():
     # configure logging
     logging.basicConfig()
     logging.root.setLevel(logging.INFO)
 
-    # global variables
     global calibration_value
     calibration_value = 0
+
 
 
 def main():
@@ -66,20 +72,16 @@ def main():
         logging.info("Line To Process: %s", line)
 
         # search line for first number character
-        match = re.findall(r"\d{1}", line)
+        match = re.findall(r"\d", line)
         logging.info("Matched Digits: %s", match)
 
-        # was a match found
-        if match is not None:
-            last_match = match.__len__()
-            logging.info("Len: %s", last_match)
+        
+        first_digit = match[0]
 
-            first_digit = match[0]
-
-            if last_match == 1:
-                last_digit = match[0]
-            else:
-                last_digit = match[last_match - 1]
+        if len(match) == 1:
+            last_digit = match[0]
+        else:
+            last_digit = match[len(match) - 1]
 
         logging.info("First Digit: %s\nSecond Digit: %s", first_digit, last_digit)
 
@@ -88,6 +90,8 @@ def main():
         calibration_value += createCalibrationValue(first_digit, last_digit)
 
         logging.info("CalibrationValue: %d", calibration_value)
+
+
 
 
 if __name__ == "__main__":
